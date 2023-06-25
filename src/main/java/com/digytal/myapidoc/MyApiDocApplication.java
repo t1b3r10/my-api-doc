175af77ba5aa985a39e6dbcbadf16de5404a5af6
Tiberio Lima <tiberio.lima@gmail.com>
2023-06-25 20:00:39 -0300

    First Commit

---
+++ src/main/java/com/digytal/myapidoc/MyApiDocApplication.java
@@ -0,0 +1,13 @@
+package com.digytal.myapidoc;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MyApiDocApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MyApiDocApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/digytal/myapidoc/MyApiDocApplicationTests.java
@@ -0,0 +1,13 @@
+package com.digytal.myapidoc;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MyApiDocApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
